d2c5fa7714f0a51ec7debe6f5041d03198d97d42
PRNC-CoOp <pc118852@gmail.com>
2026-06-02 07:38:12 +0530

    upload full project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './components/home'
+import Home from './components/Home'
 import Start from "./components/Start";
 import { Routes, Route, Link } from 'react-router-dom'
 
